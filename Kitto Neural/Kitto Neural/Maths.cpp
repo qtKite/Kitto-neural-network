ce75d16a081e920b686ab22de37c3cc1841e430b
Jacky <jackywen4@gmail.com>
2018-06-13 23:50:26 +1000

    Maths stuff

--- Kitto Neural/Kitto Neural/Maths.cpp
+++ Kitto Neural/Kitto Neural/Maths.cpp
@@ -1,1 +1,10 @@
 #include "stdafx.h"
+
+namespace Maths {
+
+	std::vector <double> transpose(float &matrix, int colummn, int rows) {
+
+
+	}
+
+}
--- Kitto Neural/Kitto Neural/Maths.h
+++ Kitto Neural/Kitto Neural/Maths.h
@@ -1,1 +1,8 @@
 #pragma once
+
+namespace Maths
+{
+	std::vector <double> transpose(float &matrix, int colummn, int rows);
+
+
+}
